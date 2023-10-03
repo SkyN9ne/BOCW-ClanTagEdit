9fbc68f7310eaf4ee84ef8ace0a609c622886f1b
SkyN9ne <82126207+SkyN9ne@users.noreply.github.com>
2023-10-03 20:51:50 +0300

    Update DllMain.cpp

--- DllMain.cpp
+++ DllMain.cpp
@@ -1,4 +1,4 @@
-// #include "pch.h"
+//#include "pch.h" // Uncomment if you want to use Pre-Compiled Headers 
 #include "stdio.h"
 #include <windows.h>
 #include <TlHelp32.h>
@@ -7,36 +7,43 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-using namespace std;
-#pragma warning(disable : 4996)
-typedef const char* (__fastcall* tLiveStats_SetClanTag)(__int64 client, const char* clantag);
-tLiveStats_SetClanTag LiveStats_SetClanTag;
-uintptr_t moduleBase = (uintptr_t)GetModuleHandle(L"BlackOpsColdWar.exe");
-__int64 TagOffset = 0x01; //NEEDS TO BE UPDATED VIA THE SIG
 
+using namespace std;
+
+#pragma warning(disable : 4996)
+
+typedef const char* (__fastcall* tLiveStats_SetClanTag)(__int64 client, const char* clantag);
+
+tLiveStats_SetClanTag LiveStats_SetClanTag;
+
+uintptr_t moduleBase = (uintptr_t)GetModuleHandle(L"BlackOpsColdWar.exe");
+__int64 TagOffset = 0x01; // OFFSET NEEDS TO BE UPDATED VIA THE SIG
 
 
 void MainThread()
 {
      while (true)
      {
-
          if (GetAsyncKeyState(VK_NUMPAD0) & 0x1U)
          {
              LiveStats_SetClanTag(0, "TAGHERE");
          }
-        
      }
 }
+
+
 void Initialize()
 {
     LiveStats_SetClanTag = (tLiveStats_SetClanTag)(TagOffset);
     CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)MainThread, NULL, NULL, NULL);
 }
+
+
 BOOL APIENTRY DllMain(HMODULE hModule,
     DWORD  ul_reason_for_call,
     LPVOID lpReserved
 )
+
 {
     switch (ul_reason_for_call)
     {
